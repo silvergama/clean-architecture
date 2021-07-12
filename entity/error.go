c8fb4dd395728fcf4273617e16866190cd1c7b13
Silver Gama <silver.mdg@gmail.com>
2021-07-12 19:17:19 -0300

    feat(entity/errors): Add errors entity

---
+++ entity/error.go
@@ -0,0 +1,21 @@
+package entity
+
+import "errors"
+
+// ErrNotFound not found
+var ErrNotFound = errors.New("Not found")
+
+// ErrInvalidEntity invalid entity
+var ErrInvalidEntity = errors.New("Invalid entity")
+
+// ErrCannotBeDeleted cannot be deleted
+var ErrCannotBeDeleted = errors.New("Cannot be deleted")
+
+// ErrEnoughBooks enough books
+var ErrNotEnoughBooks = errors.New("Not enough books")
+
+// ErrBookAlreadyBorrowed book already borrowed
+var ErrBookAlreadyBorrowe = errors.New("Books already borrowed")
+
+// ErrBookNotBorrowed book not borrowed
+var ErrBookNotBorrowed = errors.New("Book not borrowed")
