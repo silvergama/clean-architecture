4478c62008a5b8917cfec5a86ab46cc07820963f
Silver Gama <silver.mdg@gmail.com>
2021-07-19 19:58:16 -0300

    feat(usecase/user/writer): Add user writer interface (#13)

--- usecase/user/interface.go
+++ usecase/user/interface.go
@@ -3,19 +3,32 @@ package user
 
 import "github.com/silvergama/clean-architecture/entity"
 
-// Reader interface
+// Reader user reader
 type Reader interface {
 	Get(id entity.ID) (*entity.User, error)
 	Search(query string) ([]*entity.User, error)
 	List() ([]*entity.User, error)
 }
 
-type Repository interface {
-	Reader
+// Writer user writer
+type Writer interface {
+	Create(u *entity.User) (entity.ID, error)
+	Update(u *entity.User) error
+	Delete(id entity.ID) error
 }
 
+// Repository interface
+type Repository interface {
+	Reader
+	Writer
+}
+
+// UseCase interface
 type UseCase interface {
 	GetUser(id entity.ID) (*entity.User, error)
 	SearchUsers(query string) ([]*entity.User, error)
 	ListUsers() ([]*entity.User, error)
+	CreateUser(email, password, firstName, lastName string) (entity.ID, error)
+	UpdateUser(u *entity.User) error
+	DeleteUser(id entity.ID) error
 }
--- usecase/user/service.go
+++ usecase/user/service.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"strings"
+	"time"
 
 	"github.com/silvergama/clean-architecture/entity"
 )
@@ -19,14 +20,14 @@ func NewService(r Repository) *Service {
 }
 
 // CreateUser create a new user
-//func (s *Service) CreateUser(email, password, firstName, lastName string) (entity.ID, error) {
-//	u, err := entity.NewUser(email, password, firstName, lastName)
-//	if err != nil {
-//		return u.ID, err
-//	}
-//
-//	return s.repo.Create(u)
-//}
+func (s *Service) CreateUser(email, password, firstName, lastName string) (entity.ID, error) {
+	u, err := entity.NewUser(email, password, firstName, lastName)
+	if err != nil {
+		return u.ID, err
+	}
+
+	return s.repo.Create(u)
+}
 
 // GetUser get an user
 func (s *Service) GetUser(id entity.ID) (*entity.User, error) {
@@ -41,4 +42,30 @@ func (s *Service) SearchUsers(query string) ([]*entity.User, error) {
 // ListUsers list users
 func (s *Service) ListUsers() ([]*entity.User, error) {
 	return s.repo.List()
+}
+
+// UpdateUser update an user
+func (s *Service) UpdateUser(u *entity.User) error {
+	err := u.Validate()
+	if err != nil {
+		return entity.ErrInvalidEntity
+	}
+
+	u.UpdatedAt = time.Now()
+	return s.repo.Update(u)
+}
+
+// DeleteUser delete an user
+func (s *Service) DeleteUser(id entity.ID) error {
+	u, err := s.GetUser(id)
+	if u == nil {
+		return entity.ErrNotFound
+	}
+	if err != nil {
+		return err
+	}
+	if len(u.Books) > 0 {
+		return entity.ErrCannotBeDeleted
+	}
+	return s.repo.Delete(id)
 }
